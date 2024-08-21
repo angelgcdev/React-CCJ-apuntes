b6ea9b2bb43e1c5d86a7c3f3771c7d1be599b35f
Luis Gutierrez <angelgcdev@gmail.com>
2024-08-21 06:05:00 -0400

    Continuar desde el video 005 Creando el Header

---
+++ calculadora_propinas/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-9xl font-bold text-indigo-600 bg-indigo-100 ">
+        Hola Mundo
+      </h1>
+      <p>Hola Mundo 2</p>
+    </>
+  );
+}
+
+export default App;
