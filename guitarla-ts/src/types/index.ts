4176de819221310b9342c38fbd5dd45b01f564dd
Luis Gutierrez <angelgcdev@gmail.com>
2024-08-20 20:42:47 -0400

    Terminado el archivo 08-PROYECTO - Carrito de compras - Migrando a TypeScript paso a paso. Continuar desde el archivo 09-PROYECTO

---
+++ guitarla-ts/src/types/index.ts
@@ -0,0 +1,12 @@
+// Types
+export type GuitarType = {
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+};
+
+export type CartItem = GuitarType & {
+  quantity: number;
+};
