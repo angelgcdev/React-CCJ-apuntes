ae5072de74c8180b21f2b26ae676a284ed7190cd
Luis Gutierrez <angelgcdev@gmail.com>
2024-08-22 05:57:05 -0400

    Continuar desde el video 016 Formateando el Consumo y mostrando cantidades

--- calculadora_propinas/src/components/MenuItem.tsx
+++ calculadora_propinas/src/components/MenuItem.tsx
@@ -2,14 +2,14 @@ import type { MenuItem } from "../types";
 
 type MenuItemProps = {
   item: MenuItem;
-  addItem: () => void;
+  addItem: (item: MenuItem) => void;
 };
 
 export default function MenuItem({ item, addItem }: MenuItemProps) {
   return (
     <button
       className="border-2 border-teal-400 w-full p-4 flex justify-between hover:bg-teal-200 rounded-lg"
-      onClick={addItem}
+      onClick={() => addItem(item)}
     >
       <p>{item.name}</p>
       <p className="font-black">${item.price}</p>
